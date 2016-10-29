5777b70f9d1602bcd53da9237e97fcfbe745f49c
marsha17 <marsha17@seattleu.edu>
2016-10-29 17:50:50 -0400

    Flickr I

--- js/main.js
+++ js/main.js
@@ -5,7 +5,68 @@
 // of tags and view the images that are found.
 //
 // Allow users to click the images to see a larger version with more information.
-$(document).on('ready', function(){
+$(document).on('ready',function(){
+  var searchImages = function(tags){
+    // Call Flicker API //
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    console.log(tags);
+    $('#images').innerHTML = '<li class="search-throbber"<Finding...</li>';
+    // Searches for images based on user input/tags //
+    $.getJSON(flickrAPI, {
+      tags: tags,
+      tagMode: "any",
+      format: "json"
+      // Function complete //
+     }).done(function( data ){
+      $('#images').empty();
+      $('h1.search-title').first()[0].innerHTML = "Search for: " + tags;
+      $.each(data.items, function(i, item){
+        var newListItem = $("<li>")
+        var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
+        
+// Button //
+        var newButton = $("<button class='btn btn-sm btn-primary'>More Information</button>").attr({
+          'data-title': item.title,
+          'data-toggle': "modal",
+          'data-target': "#infoModal",
+          'data-imgsrc': item.media.m,
+          'data-description': item.description,
+          'type': "button"
+        }).appendTo(newListItem);
+        newListItem.appendTo("#images");
+        if(i === 60) {
+          return false;
+        }
+      });
+  });
+  };
+                    
+  // Search Button //
+     $('button.search').on('click',function(event){
+      event.preventDefault();
+      var searchTextInput = $(event.target.parentElement).find('input[name="searchText"]')[0];
+      console.log(searchTextInput);
+      searchImages(searchTextInput.value);
+    });
+  //Modal Button //
+    $('#infoModal').on('show.bs.modal',function(event){
+      var button = $(event.relatedTarget);  //Button that triggered the modal //
+      var title = button.data('title');  //Extract info from data-* attributes //
+      var imgSrc = button.data('imgsrc');
+      var imageDescription = button.data('description');
+      
+      //Update the modal's content //
+      var modal = $(this);
+      modal.find('.modal-title').html(title);
+      var modalBody = modal.find('.modal-body');
+      modalBody.empty();
+      var modalDescription = $("<p class='image-description'>").html(imageDescription).appendTo(modalBody);
+    });
+    });
+  
+  
+  
+  
     // Place your code here, inside the document ready handler.
 
     // Create a function called `searchImages()`. This function will handle the
@@ -43,7 +104,3 @@ $(document).on('ready', function(){
 
     // STRETCH GOAL: Add a "more info" popup using the technique shown on the
     // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-
-
-
-});
